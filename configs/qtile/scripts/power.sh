8f6ac6c4f27eb512418128f1eede45e8e377c8e4
MD.Ahsan Habib <68529306+BIBJAW@users.noreply.github.com>
2023-05-27 23:54:41 +0600

    Add files via upload

--- configs/qtile/scripts/power.sh
+++ configs/qtile/scripts/power.sh
@@ -1,12 +1,11 @@
 #! /bin/sh
 
-chosen=$(printf "Lock\nLog Out\nSuspend\nRestart\nOFF" | rofi -dmenu -i -theme-str '@import "~/.config/qtile/scripts/powermenu.rasi"')
+chosen=$(printf "Log Out\nSuspend\nRestart\nPower OFF" | rofi -dmenu -i -theme-str '@import "~/.config/rofi/powermenu.rasi"')
 
 case "$chosen" in
-    "Lock") betterlockscreen -l dimblur;;
 	"Log Out") killall qtile ;;
 	"Suspend") i3lock -i ~/Pictures/backgrounds/coffee.png && systemctl suspend ;;
 	"Restart") systemctl reboot ;;
-	"OFF") systemctl poweroff ;;
+	"Power OFF") systemctl poweroff ;;
 	*) exit 1 ;;
 esac
